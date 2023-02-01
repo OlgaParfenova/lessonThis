6693a4a4c79b6f87aec448f37071c5aea28a072b
Olga Parfenova <work.parfenova@gmail.com>
2023-02-01 15:15:08 +0300

    Call, bind, apply methods revision

---
+++ script.js
@@ -0,0 +1,19 @@
+const myFunc = function(name, age) {
+    return this.name + ", " + this.age;
+}
+
+const user= {
+    name: 'Harry Potter',
+    getName: myFunc,
+};
+
+// console.log(user.getName());
+
+let newMyFunc = myFunc.bind({name: 'John'}); // создание функции с привязанным контекстом, аргументы передаются через запятую.
+newMyFunc = myFunc.bind({name: 'Sara', age: 23}); // измененние значения переменной
+console.log(newMyFunc.call({name: 'Anna', age: 38})); // второй раз контекст не привязывается
+
+console.log(myFunc.call({name: 'Peter Parker', age: 26})); // вызов функции с привязанным контекстом, аргументы передаются через запятую.
+
+console.log(myFunc.apply({name: 'Jack Sparrow', age: 26})); // создание функции с привязанным контекстом, аргументы передаются как элементы массива.
+
